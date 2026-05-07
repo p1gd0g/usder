bf8d855de60f82cfe76e61ad0fe41c5c342d675a
p1gd0g <renhao.szu@gmail.com>
2026-05-07 17:52:29 +0800

    Update Flutter engine revision and version number
    - Updated the engine revision in flutter_bootstrap.js and index.html to "42d3d75a56efe1a2e9902f52dc8006099c45d937".
    - Updated the canvasKit URL in main.dart.js to reflect the new engine revision.
    - Bumped the app version in version.json from "0.5.0" to "0.5.1".
    - Updated the wimp.wasm binary file.

--- public/main.dart.js
+++ public/main.dart.js
@@ -32354,7 +32354,7 @@ $S:2}
 A.aQ3.prototype={
 $1(a){var s=A.dS().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/e4b8dca3f1b4ede4c30371002441c88c12187ed6/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/42d3d75a56efe1a2e9902f52dc8006099c45d937/":s)+a},
 $S:35}
 A.O3.prototype={
 ZP(){var s=this.z0(),r=$.bs.c3().ImageFilter.MakeColorFilter(s,null)
